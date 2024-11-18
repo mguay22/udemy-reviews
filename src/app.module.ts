551ebaf37e92ead415fb34a176bf8bd5d82f6812
Michael Guay <michaelguay@Michaels-Mac-Studio.local>
2024-11-17 23:16:41 -0500

    Pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [ReviewsModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ReviewsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    LoggerModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
   app.enableCors();
   await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
